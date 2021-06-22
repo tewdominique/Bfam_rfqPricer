9b3b4cdba9f588b4150e3398c23e6f1fa57253f5
tewdominique <tewdominique@gmail.com>
2021-06-23 01:55:49 +0800

    First Commit Synchronous Version

---
+++ Domain/RfqPricerEngine.cs
@@ -0,0 +1,37 @@
+﻿using RfqParser.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RfqParser.Domain
+{
+    public class RfqPricerEngine
+    {
+
+        private IQuoteCalculationEngine _quoteCalculationEngine;
+        private IReferencePriceSource _referencePriceSource;
+        //private QuoteCalculationEngine _quoteCalculationEngine;
+
+        public RfqPricerEngine(IQuoteCalculationEngine quoteCalculationEngine,IReferencePriceSource referencePriceSource)
+        {
+            _quoteCalculationEngine = quoteCalculationEngine;
+            _referencePriceSource = referencePriceSource;
+        }
+
+        public double PricingOrchestration(int securityId, bool buy, int quantity)
+        {
+
+            double refPrice = _referencePriceSource.get(securityId);
+            //make the calculation async
+            double quotedPrice = _quoteCalculationEngine.calculateQuotePrice(securityId, refPrice, buy, quantity);
+
+            return quotedPrice;
+        }
+
+        public double PricingOrchestration(Rfq rfq)
+        {
+            return PricingOrchestration(rfq.SecurityId, rfq.Buy, rfq.Quantity);
+        }
+    }
+}
---
+++ Infrastructure/ReferencePriceSource.cs
@@ -0,0 +1,26 @@
+﻿using RfqParser.Infrastructure;
+
+namespace RfqParser.Domain
+{
+    public class ReferencePriceSource : IReferencePriceSource
+    {
+        //private readonly IReferencePriceSourceListener _referencePriceSourceListener;
+
+        //public ReferencePriceSource(IReferencePriceSourceListener referencePriceSourceListener)
+        //{
+        //    _referencePriceSourceListener = referencePriceSourceListener;
+        //}
+
+        public void subscribe(IReferencePriceSourceListener listener)
+        {
+            return;
+        }
+
+        public double get(int securityId)
+        {
+            double refPrice = 0.0;
+
+            return refPrice;
+        }
+    }
+}
